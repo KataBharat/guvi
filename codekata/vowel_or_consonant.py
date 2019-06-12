7e776e2a72c76b57b771559f4b59eb5415317bd1
KataBharat <51686288+KataBharat@users.noreply.github.com>
2019-06-12 10:40:03 +0530

    Update vowel_or_consonant.py

--- codekata/vowel_or_consonant.py
+++ codekata/vowel_or_consonant.py
@@ -1,1 +1,12 @@
+c = input()
 
+v = ['a','e','i','o','u']
+con = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z']
+
+if(c in v):
+    print("Vowel")
+    exit
+if(c in con):
+    print("Consonant")
+else:
+    print("invalid")
