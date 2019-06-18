6c39611bc82ad70f193f7b0b989b2617cbb5aadf
KataBharat <51686288+KataBharat@users.noreply.github.com>
2019-06-18 10:50:51 +0530

    Create roman_to_integer.py

---
+++ codekata/roman_to_integer.py
@@ -0,0 +1,5 @@
+s1=input()
+s2=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+  if(s1==s2[i]):
+    print(i)
