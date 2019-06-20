c408367f202d11a01653f486c0c96bb1b13ed319
KataBharat <51686288+KataBharat@users.noreply.github.com>
2019-06-20 10:38:53 +0530

    Update differ_in_string_by_1_char(1).py

--- codekata/differ_in_string_by_1_char(1).py
+++ codekata/differ_in_string_by_1_char(1).py
@@ -1,9 +1,9 @@
 s11, s22 = input().split()
-count = 0
+count1 = 0
 for i in range(len(s11)):
   if s11[i]!=s22[i]:
-    count=count+1
-if count==1:
+    count1=count1+1
+if count1==1:
   print ("yes")
 else:
   print ("no")
