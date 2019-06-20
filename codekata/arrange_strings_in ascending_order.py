b976b5088aa5ced5c423ddd3c034b6e1f73c56d7
KataBharat <51686288+KataBharat@users.noreply.github.com>
2019-06-20 10:07:52 +0530

    Update arrange_strings_in ascending_order.py

--- codekata/arrange_strings_in ascending_order.py
+++ codekata/arrange_strings_in ascending_order.py
@@ -2,12 +2,14 @@ s=int(input())
 x = [(x) for x in input().split()]
 x = sorted(x, key=len)
 for i in range(0,s-1):
-    if(len(x[i])==len(x[i+1])): #arranging the words with same length lexicographically
+    if(len(x[i])==len(x[i+1])):
         t=x[i]
         p=x[i+1]
-        if(t[0]>p[0]):
-            x[i+1]=t
-            x[i]=p
-        else:
-            continue
+        for i in range(0,len(t)): #arranging strings of same length lexicographly
+            if(t[i]>p[i]):
+                x[i+1]=t
+                x[i]=p
+                continue
+            else:
+                continue
 print(*x,sep=' ')
