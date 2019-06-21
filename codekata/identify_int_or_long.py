cb3854a95d27a989c6382454a912cddf28c3bff7
KataBharat <51686288+KataBharat@users.noreply.github.com>
2019-06-21 12:13:44 +0530

    Update identify_int_or_long.py

--- codekata/identify_int_or_long.py
+++ codekata/identify_int_or_long.py
@@ -1,2 +1,7 @@
-s=int(input())
-print("INT")
+n1 = int(input())
+if n1 >= -32767 and n1 <= 32767:
+  print("INT")
+elif n1 >= -2147483647 and n1 <= 2147483647:
+  print("LONG")
+elif n1 >= -9223372036854775807 and n1 <= 9223372036854775807:
+  print("LONG LONG")
