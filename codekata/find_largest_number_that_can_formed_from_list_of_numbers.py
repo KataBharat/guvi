4d1e987fd2c7db0cd5f0cd11963dd01c432dabd1
KataBharat <51686288+KataBharat@users.noreply.github.com>
2019-07-27 21:34:12 +0530

    Update find_largest_number_that_can_formed_from_list_of_numbers.py

--- codekata/find_largest_number_that_can_formed_from_list_of_numbers.py
+++ codekata/find_largest_number_that_can_formed_from_list_of_numbers.py
@@ -3,5 +3,9 @@ s=[int(x) for x in input().split()]
 s=sorted(s)
 k=''
 for i in range(len(s)-1,-1,-1):
-    k=k+str(s[i])
-print(k)
+    if(s[i]!=0):
+        k=k+str(s[i])
+if(len(k)>0):
+    print(k)
+else:
+    print("0")
